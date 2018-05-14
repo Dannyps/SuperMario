7ef0b9a2c8002b33976db7763c0ca4390e0a41fe
mcarolinaSoares <up201605010@fe.up.pt>
2018-05-14 23:47:50 +0100

    Lamp solved

--- core/src/com/mygame/mario/Levels/BaseScreen.java
+++ core/src/com/mygame/mario/Levels/BaseScreen.java
@@ -6,10 +6,17 @@ import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -53,6 +60,20 @@ public class BaseScreen implements Screen {
         map = maploader.load("level1m.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
         gameCam.position.set(gamePort.getScreenWidth()/2, gamePort.getScreenHeight()/2, 0);
+
+        world = new World (new Vector2(0, 0), true);
+        b2dr = new Box2DDebugRenderer();
+
+        BodyDef bodyd = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fixtured = new FixtureDef();
+        Body body;
+
+        for (MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class))
+        {
+
+        }
+
     }
 
     @Override
