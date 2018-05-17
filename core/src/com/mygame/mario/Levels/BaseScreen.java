27a7ccd97ddba14369e550e41b09f5c427533f6e
mcarolinaSoares <up201605010@fe.up.pt>
2018-05-17 18:17:17 +0100

    add sprites images

--- core/src/com/mygame/mario/Levels/BaseScreen.java
+++ core/src/com/mygame/mario/Levels/BaseScreen.java
@@ -16,6 +16,7 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
